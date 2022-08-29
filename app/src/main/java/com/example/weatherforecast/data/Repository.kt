78aa77a370b81fc3da60e153aa8c17b20825f8c5
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 16:31:24 +0430

    private lateinit var hourlyForecastCache added to Repository.kt

--- app/src/main/java/com/example/weatherforecast/data/Repository.kt
+++ app/src/main/java/com/example/weatherforecast/data/Repository.kt
@@ -1,12 +1,15 @@
 package com.example.weatherforecast.data
 
 import android.location.Location
+import com.example.weatherforecast.data.model.HourlyWeatherForecast
 import javax.inject.Inject
 
 class Repository @Inject constructor(
     private val remoteDataSource: IRemoteDataSource,
     private val locationDataSource: ILocationDataSource
 ) {
+
+    private lateinit var hourlyForecastCache: List<HourlyWeatherForecast>
 
     suspend fun getCurrentLocation(): Location? = locationDataSource.getCurrentLocation()
 

91d601bd4d407fa6cc1af6601adfe47497b8fafc
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 16:28:44 +0430

    Failure added to ApiResponse.kt

--- app/src/main/java/com/example/weatherforecast/util/ApiResponse.kt
+++ app/src/main/java/com/example/weatherforecast/util/ApiResponse.kt
@@ -3,4 +3,5 @@ package com.example.weatherforecast.util
 sealed class ApiResponse<T>{
     object Loading : ApiResponse<Nothing>()
     object Success : ApiResponse<Nothing>()
+    data class Failure<T>(val message: String?) : ApiResponse<T>()
 }

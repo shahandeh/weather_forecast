e28e457acb33ba7c3cf6dbc90b4197c7587d5255
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 16:36:41 +0430

    String.timeAdapter() added to TimeUtil.kt

--- app/src/main/java/com/example/weatherforecast/util/TimeUtil.kt
+++ app/src/main/java/com/example/weatherforecast/util/TimeUtil.kt
@@ -10,3 +10,12 @@ fun currentTime(): String {
     val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm")
     return simpleDateFormat.format(calendar.time).toString().replace(" ", "T")
 }
+
+fun String.timeAdapter(): String {
+    val date = this.split("T")[0]
+    val hour = this.split("T")[1].split(":")[0]
+
+    return if (hour == "00") date
+    else if (hour.toInt() > 12) "${hour.toInt() - 12}PM"
+    else "${hour.toInt()}AM"
+}

adf723e60397ea1930844b8012482323237b96d2
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 16:36:24 +0430

    currentTime() added to TimeUtil.kt

--- app/src/main/java/com/example/weatherforecast/util/TimeUtil.kt
+++ app/src/main/java/com/example/weatherforecast/util/TimeUtil.kt
@@ -1,2 +1,12 @@
 package com.example.weatherforecast.util
 
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
+@SuppressLint("SimpleDateFormat")
+fun currentTime(): String {
+    val calendar = Calendar.getInstance()
+    val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm")
+    return simpleDateFormat.format(calendar.time).toString().replace(" ", "T")
+}

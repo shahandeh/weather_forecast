9bb0fcb65d688a2de1eb94ab2d740f71038e2bfe
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 16:25:20 +0430

    getCurrentLocation() added to Repository.kt

--- app/src/main/java/com/example/weatherforecast/data/Repository.kt
+++ app/src/main/java/com/example/weatherforecast/data/Repository.kt
@@ -1,9 +1,13 @@
 package com.example.weatherforecast.data
 
+import android.location.Location
 import javax.inject.Inject
 
 class Repository @Inject constructor(
     private val remoteDataSource: IRemoteDataSource,
     private val locationDataSource: ILocationDataSource
 ) {
+
+    suspend fun getCurrentLocation(): Location? = locationDataSource.getCurrentLocation()
+
 }

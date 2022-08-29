91f9f1d231128983446640f54a194a4891ffa874
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 16:17:56 +0430

    getForecast() added to IRemoteDataSource.kt

--- app/src/main/java/com/example/weatherforecast/data/IRemoteDataSource.kt
+++ app/src/main/java/com/example/weatherforecast/data/IRemoteDataSource.kt
@@ -1,4 +1,13 @@
 package com.example.weatherforecast.data
 
+import com.example.weatherforecast.data.model.ForecastApiResponse
+import retrofit2.Response
+
 interface IRemoteDataSource {
+
+    suspend fun getForecast(
+        latitude: Double,
+        longitude: Double
+    ): Response<ForecastApiResponse>
+
 }

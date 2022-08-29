04bfd06c43501be85852e776e1a2e66b3960f25e
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 16:11:35 +0430

    provideBaseUrl() added to RemoteDataSourceModule.kt

--- app/src/main/java/com/example/weatherforecast/di/RemoteDataSourceModule.kt
+++ app/src/main/java/com/example/weatherforecast/di/RemoteDataSourceModule.kt
@@ -1,10 +1,17 @@
 package com.example.weatherforecast.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RemoteDataSourceModule {
+
+    @Singleton
+    @Provides
+    fun provideBaseUrl(): String = "https://api.open-meteo.com/v1/"
+
 }

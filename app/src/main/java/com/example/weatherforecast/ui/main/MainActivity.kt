731bcad7576165bb17c83d22c41a946db8200c73
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 15:38:05 +0430

    MainActivity.kt moved to .ui.main

--- app/src/main/java/com/example/weatherforecast/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.weatherforecast
+package com.example.weatherforecast.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weatherforecast.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

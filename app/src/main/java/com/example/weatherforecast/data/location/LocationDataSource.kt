eeb01b57b518a1066d9756b7bb0ba044fc5018cc
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 16:01:27 +0430

    getCurrentLocation() override in LocationDataSource.kt

--- app/src/main/java/com/example/weatherforecast/data/location/LocationDataSource.kt
+++ app/src/main/java/com/example/weatherforecast/data/location/LocationDataSource.kt
@@ -1,12 +1,20 @@
 package com.example.weatherforecast.data.location
 
+import android.Manifest
 import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationManager
+import android.os.Looper
+import androidx.core.content.ContextCompat
 import com.example.weatherforecast.data.ILocationDataSource
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
+import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
+import kotlin.coroutines.resume
 
 class LocationDataSource @Inject constructor(
     private val fusedLocationProviderClient: FusedLocationProviderClient,
@@ -16,7 +24,38 @@ class LocationDataSource @Inject constructor(
 ) : ILocationDataSource {
 
     override suspend fun getCurrentLocation(): Location? {
-        TODO("Not yet implemented")
+
+        val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(
+            application,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            application,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val locationManager =
+            application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+        if (!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled) {
+            return null
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+
+        return suspendCancellableCoroutine { cont ->
+            fusedLocationProviderClient.lastLocation.addOnCompleteListener {
+                cont.resume(it.result)
+                    .also { fusedLocationProviderClient.removeLocationUpdates(locationCallback) }
+            }
+        }
     }
-    
 }

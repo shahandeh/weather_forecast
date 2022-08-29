bb58e52ccdc72ccd0a882be40b8f4670a61b06e9
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 16:21:04 +0430

    RemoteDataSource.kt implement IRemoteDataSource

--- app/src/main/java/com/example/weatherforecast/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/example/weatherforecast/data/remote/RemoteDataSource.kt
@@ -1,9 +1,19 @@
 package com.example.weatherforecast.data.remote
 
+import com.example.weatherforecast.data.IRemoteDataSource
+import com.example.weatherforecast.data.model.ForecastApiResponse
 import com.example.weatherforecast.data.remote.service.ApiService
+import retrofit2.Response
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val apiService: ApiService
-){
+): IRemoteDataSource {
+    override suspend fun getForecast(
+        latitude: Double,
+        longitude: Double
+    ): Response<ForecastApiResponse> {
+        TODO("Not yet implemented")
+    }
+
 }

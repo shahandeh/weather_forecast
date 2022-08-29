4ae2d85ad02603929428e07e3c24b471c28e0461
majid <majid.shahandeh.ms@gmail.com>
2022-08-29 15:53:54 +0430

    provideGetFusedLocation() added to LocationDataSourceModule.kt

--- app/src/main/java/com/example/weatherforecast/di/LocationDataSourceModule.kt
+++ app/src/main/java/com/example/weatherforecast/di/LocationDataSourceModule.kt
@@ -1,10 +1,23 @@
 package com.example.weatherforecast.di
 
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object LocationDataSourceModule {
+
+    @Singleton
+    @Provides
+    fun provideGetFusedLocation(
+        @ApplicationContext context: Context
+    ): FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
 }

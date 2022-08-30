41f5ba8c84721380289d67715c9d6027044b6bd6
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 19:01:46 +0430

    init() added to CustomViewFetchData.kt

--- app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewFetchData.kt
+++ app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewFetchData.kt
@@ -3,11 +3,17 @@ package com.example.weatherforecast.ui.customview
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.weatherforecast.R
 import com.example.weatherforecast.databinding.CustomViewFetchDataBinding
 
 class CustomViewFetchData(ctx: Context, attrs: AttributeSet) : ConstraintLayout(ctx, attrs) {
 
     private var binding: CustomViewFetchDataBinding
+
+    init {
+        val view = inflate(ctx, R.layout.custom_view_fetch_data, this)
+        binding = CustomViewFetchDataBinding.bind(view)
+    }
 
 
 }

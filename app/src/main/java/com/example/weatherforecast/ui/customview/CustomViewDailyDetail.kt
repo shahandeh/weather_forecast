b82114b0784cd7766a13ac4e04517158296f5b04
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 18:28:26 +0430

    private var binding added to CustomViewDailyDetail.kt

--- app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewDailyDetail.kt
+++ app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewDailyDetail.kt
@@ -3,5 +3,10 @@ package com.example.weatherforecast.ui.customview
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.weatherforecast.databinding.CustomViewDailyDetailBinding
 
-class CustomViewDailyDetail(ctx: Context, attrs: AttributeSet) : ConstraintLayout(ctx, attrs) {}
+class CustomViewDailyDetail(ctx: Context, attrs: AttributeSet) : ConstraintLayout(ctx, attrs) {
+
+    private var binding: CustomViewDailyDetailBinding
+
+}

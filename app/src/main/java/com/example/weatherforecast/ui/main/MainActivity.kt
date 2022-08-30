530d0ba8001aa287596380bd988e4ed8d16f9087
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 19:08:26 +0430

    private lateinit var binding added to MainActivity.kt

--- app/src/main/java/com/example/weatherforecast/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/ui/main/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.weatherforecast.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weatherforecast.R
+import com.example.weatherforecast.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

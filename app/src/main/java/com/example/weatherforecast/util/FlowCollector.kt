9f1085671dbde52c44bf773c3e5a30480c9f7fd6
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 20:13:38 +0430

    CoroutineScope.flowCollector() added to FlowCollector.kt

--- app/src/main/java/com/example/weatherforecast/util/FlowCollector.kt
+++ app/src/main/java/com/example/weatherforecast/util/FlowCollector.kt
@@ -1,2 +1,13 @@
 package com.example.weatherforecast.util
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+fun <T> CoroutineScope.flowCollector(flow: Flow<T?>, fn: (value: T) -> Unit){
+    launch {
+        flow.collect{
+            if (it != null) fn(it)
+        }
+    }
+}

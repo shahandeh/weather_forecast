45f62c3a84fa4104b485dde92b77eb85d78c9dfd
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 20:01:12 +0430

    getCurrentHourForecast() added to HomeViewModel.kt

--- app/src/main/java/com/example/weatherforecast/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/weatherforecast/ui/home/HomeViewModel.kt
@@ -1,12 +1,31 @@
 package com.example.weatherforecast.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.weatherforecast.data.Repository
+import com.example.weatherforecast.data.model.HourlyWeatherForecast
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val repository: Repository
 ): ViewModel() {
+
+
+
+    private var _currentHourForecast: MutableStateFlow<HourlyWeatherForecast?> =
+        MutableStateFlow(null)
+    val currentHourForecast: StateFlow<HourlyWeatherForecast?> = _currentHourForecast
+    private fun getCurrentHourForecast() {
+        viewModelScope.launch {
+            repository.currentHourForecast().collect {
+                _currentHourForecast.emit(it)
+            }
+        }
+    }
+
 }

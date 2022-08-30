4b94fe8b989501a28fe5986893e7abfc9e06609f
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 17:19:16 +0430

    CustomViewCurrentHourDetail.kt added to .ui.customview

---
+++ app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewCurrentHourDetail.kt
@@ -0,0 +1,9 @@
+package com.example.weatherforecast.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CustomViewCurrentHourDetail(ctx: Context, attrs: AttributeSet) :
+    ConstraintLayout(ctx, attrs) {
+}

d95abe1f7ecd3e300d383ca403eaf2154b74d4c2
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 19:01:29 +0430

    private var binding added to CustomViewFetchData.kt

--- app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewFetchData.kt
+++ app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewFetchData.kt
@@ -3,6 +3,11 @@ package com.example.weatherforecast.ui.customview
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.weatherforecast.databinding.CustomViewFetchDataBinding
 
 class CustomViewFetchData(ctx: Context, attrs: AttributeSet) : ConstraintLayout(ctx, attrs) {
+
+    private var binding: CustomViewFetchDataBinding
+
+
 }

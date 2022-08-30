f9c1f98a58bac8af8efad4b745db4adca3cc391c
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 17:43:02 +0430

    init() added to CustomViewCurrentHourDetail.kt

--- app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewCurrentHourDetail.kt
+++ app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewCurrentHourDetail.kt
@@ -3,11 +3,17 @@ package com.example.weatherforecast.ui.customview
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.weatherforecast.R
 import com.example.weatherforecast.databinding.CustomViewCurrentHourTempBinding
 
 class CustomViewCurrentHourDetail(ctx: Context, attrs: AttributeSet) :
     ConstraintLayout(ctx, attrs) {
 
     private var binding: CustomViewCurrentHourTempBinding
+
+    init {
+        val view = inflate(ctx, R.layout.custom_view_current_hour_temp, this)
+        binding = CustomViewCurrentHourTempBinding.bind(view)
+    }
 
 }

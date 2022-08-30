2c0fe4ffb39ff8974273d4523672f6f1b98d3df8
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 20:15:58 +0430

    <T>ApiResponse<T>.apiResponseHandler() added to ApiResponseHandler.kt

--- app/src/main/java/com/example/weatherforecast/util/ApiResponseHandler.kt
+++ app/src/main/java/com/example/weatherforecast/util/ApiResponseHandler.kt
@@ -1,2 +1,9 @@
 package com.example.weatherforecast.util
 
+fun <T>ApiResponse<T>.apiResponseHandler(loading: () -> Unit, success: () -> Unit, failure: (message: String?) -> Unit){
+    when (this) {
+        ApiResponse.Loading -> loading()
+        ApiResponse.Success -> success()
+        is ApiResponse.Failure -> failure(this.message)
+    }
+}

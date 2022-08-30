423791e26b4acc14c6fde6fa81b0b7e69b7f706b
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 18:28:39 +0430

    private var dayList added to CustomViewDailyDetail.kt

--- app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewDailyDetail.kt
+++ app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewDailyDetail.kt
@@ -3,10 +3,12 @@ package com.example.weatherforecast.ui.customview
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherforecast.databinding.CustomViewDailyDetailBinding
 
 class CustomViewDailyDetail(ctx: Context, attrs: AttributeSet) : ConstraintLayout(ctx, attrs) {
 
     private var binding: CustomViewDailyDetailBinding
+    private var dayList: RecyclerView
 
 }

18522615e1486fd85815e9d1b91b889ad9d2a385
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 19:32:32 +0430

    inner class DailyTempViewHolder added to DailyTempListAdapter.kt

--- app/src/main/java/com/example/weatherforecast/ui/adapter/DailyTempListAdapter.kt
+++ app/src/main/java/com/example/weatherforecast/ui/adapter/DailyTempListAdapter.kt
@@ -1,6 +1,15 @@
 package com.example.weatherforecast.ui.adapter
 
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherforecast.databinding.ItemViewDailyTempBinding
+
 class DailyTempListAdapter(
     private val fn: (time: String) -> Unit
 ) {
+
+    inner class DailyTempViewHolder(private val binding: ItemViewDailyTempBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
 }

7db76f518667fd776cee36c59119fabb45c31a04
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 19:39:03 +0430

    bind() added to DayListAdapter.kt

--- app/src/main/java/com/example/weatherforecast/ui/adapter/DayListAdapter.kt
+++ app/src/main/java/com/example/weatherforecast/ui/adapter/DayListAdapter.kt
@@ -1,6 +1,27 @@
 package com.example.weatherforecast.ui.adapter
 
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherforecast.data.model.DailyWeatherForecast
+import com.example.weatherforecast.databinding.ItemViewDailyDetailDayViewBinding
+
 class DayListAdapter(
     private val fn: (date: String) -> Unit
 ) {
+
+    inner class DayViewHolder(private val binding: ItemViewDailyDetailDayViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(dailyWeatherForecast: DailyWeatherForecast) {
+            val date = dailyWeatherForecast.date.split("-")
+            binding.apply {
+                day.text = date[2]
+                month.text = date[1]
+                year.text = date[0]
+            }
+            binding.root.setOnClickListener {
+                fn(dailyWeatherForecast.date)
+            }
+        }
+
+    }
 }

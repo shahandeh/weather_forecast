f8402c9d73ff4ffad8ad15958440074c9d401cc9
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 20:05:20 +0430

    private lateinit var binding added to HomeFragment.kt

--- app/src/main/java/com/example/weatherforecast/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherforecast/ui/home/HomeFragment.kt
@@ -3,11 +3,13 @@ package com.example.weatherforecast.ui.home
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.weatherforecast.R
+import com.example.weatherforecast.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment: Fragment(R.layout.fragment_home) {
 
     private val homeViewModel by viewModels<HomeViewModel>()
+    private lateinit var binding: FragmentHomeBinding
 
 }

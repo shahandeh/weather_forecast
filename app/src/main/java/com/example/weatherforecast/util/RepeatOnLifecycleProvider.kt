d48a0981d3db91fc70251af9dc958629099d021e
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 20:12:52 +0430

    Fragment.repeatOnLifecycleProvider() added to RepeatOnLifecycleProvider.kt

--- app/src/main/java/com/example/weatherforecast/util/RepeatOnLifecycleProvider.kt
+++ app/src/main/java/com/example/weatherforecast/util/RepeatOnLifecycleProvider.kt
@@ -1,2 +1,16 @@
 package com.example.weatherforecast.util
 
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+inline fun Fragment.repeatOnLifecycleProvider(crossinline fn: CoroutineScope.() -> Unit){
+    this.viewLifecycleOwner.lifecycleScope.launch{
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED){
+            fn()
+        }
+    }
+}

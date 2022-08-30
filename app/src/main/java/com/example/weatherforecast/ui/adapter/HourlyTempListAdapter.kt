99e7fcd0a4c5010116a0fb2cf17883103d4a117f
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 19:43:11 +0430

    inner class HourlyTempViewHolder() added to HourlyTempListAdapter.kt

--- app/src/main/java/com/example/weatherforecast/ui/adapter/HourlyTempListAdapter.kt
+++ app/src/main/java/com/example/weatherforecast/ui/adapter/HourlyTempListAdapter.kt
@@ -1,8 +1,14 @@
 package com.example.weatherforecast.ui.adapter
 
+import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherforecast.data.model.HourlyWeatherForecast
+import com.example.weatherforecast.databinding.ItemViewHourlyTempBinding
 
 class HourlyTempListAdapter(
     private val fn: (hourlyWeatherForecast: HourlyWeatherForecast) -> Unit
 ) {
+
+    inner class HourlyTempViewHolder(private val binding: ItemViewHourlyTempBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
 }

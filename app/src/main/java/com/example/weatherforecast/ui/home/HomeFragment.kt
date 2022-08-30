8c9a10518c47408082ff4b343695e14bb0d6f729
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 20:05:08 +0430

    private val homeViewModel added to HomeFragment.kt

--- app/src/main/java/com/example/weatherforecast/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherforecast/ui/home/HomeFragment.kt
@@ -1,9 +1,13 @@
 package com.example.weatherforecast.ui.home
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.weatherforecast.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private val homeViewModel by viewModels<HomeViewModel>()
+
 }

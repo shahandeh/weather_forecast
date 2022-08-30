4cc05cf8d8c87743d734820bc592549981f73d38
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 19:07:51 +0430

    AppCompatActivity.permissionHandler() added to PermissionHandler.kt

--- app/src/main/java/com/example/weatherforecast/util/PermissionHandler.kt
+++ app/src/main/java/com/example/weatherforecast/util/PermissionHandler.kt
@@ -1,2 +1,22 @@
 package com.example.weatherforecast.util
 
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+fun AppCompatActivity.permissionHandler(
+    permission: Array<String>,
+    granted: () -> Unit,
+    notGranted: () -> Unit
+) {
+    val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) {
+        for (i in it) {
+            if (!i.value) notGranted()
+        }
+        granted()
+    }
+    activityResultLauncher.launch(
+        permission
+    )
+}

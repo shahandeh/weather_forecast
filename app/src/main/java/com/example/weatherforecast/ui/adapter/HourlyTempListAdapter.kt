ac900738a135d7439a52e80d7ccdd164cf7a74a8
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 19:52:31 +0430

    bind() added to inner class HourlyTempViewHolder() in HourlyTempListAdapter.kt

--- app/src/main/java/com/example/weatherforecast/ui/adapter/HourlyTempListAdapter.kt
+++ app/src/main/java/com/example/weatherforecast/ui/adapter/HourlyTempListAdapter.kt
@@ -3,12 +3,25 @@ package com.example.weatherforecast.ui.adapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherforecast.data.model.HourlyWeatherForecast
 import com.example.weatherforecast.databinding.ItemViewHourlyTempBinding
+import com.example.weatherforecast.util.getWeatherImage
+import com.example.weatherforecast.util.timeAdapter
 
 class HourlyTempListAdapter(
     private val fn: (hourlyWeatherForecast: HourlyWeatherForecast) -> Unit
 ) {
 
-    inner class HourlyTempViewHolder(private val binding: ItemViewHourlyTempBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class HourlyTempViewHolder(private val binding: ItemViewHourlyTempBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(hourlyWeatherForecast: HourlyWeatherForecast) {
+            binding.hour.text = hourlyWeatherForecast.time.timeAdapter()
+            binding.temp.text = hourlyWeatherForecast.temperature
+            binding.image.getWeatherImage(
+                hourlyWeatherForecast.weatherCode,
+                hourlyWeatherForecast.time.timeAdapter()
+            )
+            binding.root.setOnClickListener { fn(hourlyWeatherForecast) }
+        }
 
     }
 }

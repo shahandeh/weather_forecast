7291c0e35d1be757020c7b4fd77f9e3bd9b65434
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 18:46:54 +0430

    CustomViewFetchData.kt added to .ui.customview

---
+++ app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewFetchData.kt
@@ -0,0 +1,8 @@
+package com.example.weatherforecast.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CustomViewFetchData(ctx: Context, attrs: AttributeSet) : ConstraintLayout(ctx, attrs) {
+}

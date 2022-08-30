4a140f35a5b9e1d57e97fa02f88834b405ca0b28
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 21:04:37 +0430

    binding initialized in onCreate()

--- app/src/main/java/com/example/weatherforecast/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/ui/main/MainActivity.kt
@@ -30,7 +30,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     private fun navHostFragmentInit() {

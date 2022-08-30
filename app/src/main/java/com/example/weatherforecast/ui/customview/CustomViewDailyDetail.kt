8bf2237ee56fbb3fdb9527361250328d6c5183fd
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 17:50:56 +0430

    CustomViewDailyDetail.kt added to .ui.customview

---
+++ app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewDailyDetail.kt
@@ -0,0 +1,7 @@
+package com.example.weatherforecast.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CustomViewDailyDetail(ctx: Context, attrs: AttributeSet) : ConstraintLayout(ctx, attrs) {}

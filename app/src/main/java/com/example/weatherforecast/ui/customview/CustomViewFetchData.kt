ed2a90867e2d6d655f7013df703ab58434179871
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 19:02:00 +0430

    fetching() added to CustomViewFetchData.kt

--- app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewFetchData.kt
+++ app/src/main/java/com/example/weatherforecast/ui/customview/CustomViewFetchData.kt
@@ -5,6 +5,8 @@ import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.weatherforecast.R
 import com.example.weatherforecast.databinding.CustomViewFetchDataBinding
+import com.example.weatherforecast.util.gone
+import com.example.weatherforecast.util.visible
 
 class CustomViewFetchData(ctx: Context, attrs: AttributeSet) : ConstraintLayout(ctx, attrs) {
 
@@ -13,6 +15,18 @@ class CustomViewFetchData(ctx: Context, attrs: AttributeSet) : ConstraintLayout(
     init {
         val view = inflate(ctx, R.layout.custom_view_fetch_data, this)
         binding = CustomViewFetchDataBinding.bind(view)
+    }
+
+    fun fetching() {
+        binding.apply {
+            lottie.playAnimation()
+            lottie.visible()
+            fetchMessage.visible()
+            appName.visible()
+            failureMessage.gone()
+            failureNote.gone()
+            tryAgain.gone()
+        }
     }
 
 

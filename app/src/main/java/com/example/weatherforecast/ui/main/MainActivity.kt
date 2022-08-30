18d118419139e7627c15e702367fe6768d6716dd
majid <majid.shahandeh.ms@gmail.com>
2022-08-30 21:02:51 +0430

    navHostFragmentInit() added to MainActivity.kt

--- app/src/main/java/com/example/weatherforecast/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/ui/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.weatherforecast.ui.main
 import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
 import com.example.weatherforecast.R
 import com.example.weatherforecast.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,4 +22,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun navHostFragmentInit() {
+        val host = NavHostFragment.create(R.navigation.navigation)
+        supportFragmentManager.commit {
+            add(R.id.container, host)
+            setPrimaryNavigationFragment(host)
+        }
+    }
+
 }
